80ba6d17e4b11c131c0bc0a1813605bbb9bb8156
JohnnyZhang7 <zjohnnyzhang@gmail.com>
2025-02-06 09:23:55 -0500

    2/6/25 Strings

--- app.py
+++ app.py
@@ -7,29 +7,62 @@ graduated = False
 #Floats for decimal
 money = 4.50
 
-def add(x,y):
+""" def add(x,y):
     print(round(x + y))
-""" add(54, "11") """
+add(54,+ "11") """
 #example of not good problem
 
 #input seeks user response and outputs that into the variable
 #Bill value will be equal to the response of the user
 #input result is a string
+
 """ bill = float(input("How much was the bill?"))
 add(15, bill) """
-
 #Lists which store lists of data
-students = ["u", "me", "that", "them", "gaf", "gar"]
+""" students = ["u", "me", "that", "them", "gaf", "gar"] """
 #similar to for i in range(4)
 #scalable, don't have to change the range all the time
-for student in students:
-    print(student)
+""" for student in students:
+    print(student) """
 
 #adding the latter to the former
-moneys = [1,2,3,4,5,6]
+""" moneys = [1,2,3,4,5,6]
 for student in students:
     print(student)
-x=0
-for money in moneys:
+x=0 """
+""" for money in moneys:
     x = x + money
-    print(x)
+    print(x) """
+
+
+
+
+
+
+
+""" x = 3
+y = float(3)
+print(x,y) """
+
+""" values = [1,2.23,5,7,2,30,15]
+print(values)
+for i in values:
+    print(i) """
+
+""" print(values[0])
+print(values[6])  """   #prints the first(1) and sixth number(15)
+
+""" "test"
+["t","e","s","t"] """
+
+""" x = "this is a thing"
+y= x.split( )
+z = y[0]
+print(y)
+print(z) """
+
+word = string = "a b c d e"
+words = string.split()         #splits the string and returns how many words
+word_count = len(words)     #counts the number of elements in the list
+print(word)
+print(word_count)
